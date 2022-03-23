7eea4a0b71a620bc1dd65bc6fd271c874256bde8
Eric Zhang <ericspring08@gmail.com>
2022-03-22 19:03:32 -0500

    created a leaderboard and two cards for selection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react'
-import {Card} from '@mui/material'
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card'
+import { CardActionArea, CardContent, Grid } from '@mui/material';
 
 import firebase from 'firebase'
 
@@ -21,9 +30,14 @@ firebase.analytics()
 
 const db = firebase.firestore()
 
+var first = "";
+var second = "";
+
+
 function App() {
 
   const [quotes, setQuotes] = useState([])
+
 
   function generateRandomQuotes(max) {
     const index = Math.floor(Math.random() * (max))
@@ -42,25 +56,66 @@ function App() {
     )
   }, [])
 
+
   return (
     <div className="App">
       <h1>Wall of Sus</h1>
       <br>
       </br>
-
-      {
-        quotes.map(item => 
-          <div>
-            <Card>
-              <h4>{item.quote}</h4>
-              <br></br>
-              <h6>{item.user}</h6>
+      <div style = {{marginLeft: "20px", marginRight: "20px"}}>
+        <Grid container spacing = {2}>
+          <Grid item xs = {6} md = {6}>
+            <Card style = {{height: "200px"}}>
+              <CardActionArea style = {{height: "100%"}}>
+                <CardContent>
+                  
+                </CardContent>
+              </CardActionArea>
             </Card>
-            <br>
-            </br>
-          </div>
-        )
-      }
+          </Grid>
+        <Grid item xs = {6} md = {6}>
+          <Card style = {{height: "200px"}}>
+            <CardActionArea style = {{height: "100%"}}>
+              <CardContent>
+                
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        </Grid>
+      </div>
+
+      <div style = {{margin: "20px"}}>
+
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Rank</TableCell>
+              <TableCell>User</TableCell>
+              <TableCell>Quote</TableCell>
+              <TableCell>Votes</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {quotes.map((row, index) => (
+              <TableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {index+1}
+                </TableCell>
+                <TableCell>{row.user}</TableCell>
+                <TableCell>{row.quote}</TableCell>
+                <TableCell>{row.rank}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+
     </div>
   );
 }
